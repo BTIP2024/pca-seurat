bc867ef4759cd4312c580cab8ae90d9179e0887d
lbsamin2 <157775356+lbsamin2@users.noreply.github.com>
2024-08-08 13:13:36 +0800

    Update pca-seurat.R

--- R/pca-seurat.R
+++ R/pca-seurat.R
@@ -13,13 +13,13 @@ pca_seurat <- function(input){
    print(for_pca[["pca"]], dims = 1:5, nfeatures = 5)
    
    image <- Seurat::VizDimLoadings(for_pca, dims = 1:2, reduction = "pca")
-   ggplot2::ggsave(image, file = "pca.png")
+   ggplot2::ggsave(image, file = "pca.png", width = 15, height = 10)
    
    image2 <- Seurat::DimHeatmap(for_pca, dims = 1, cells = 500, balanced = TRUE)
-   ggplot2::ggsave(image2, file = "heatmap.png")
+   ggplot2::ggsave(image2, file = "heatmap.png", width = 12, height = 12)
    
    image3 <- Seurat::ElbowPlot(for_pca)
-   ggplot2::ggsave(image2, file = "elbowplot.png")
+   ggplot2::ggsave(image3, file = "elbowplot.png", width = 10)
    
    image4 <- Seurat::FindNeighbors(for_pca, dims= 1:15)
    image4 <- Seurat::FindClusters(image4, resolution = c(0.1, 0.3, 0.5, 0.7, 1))
@@ -27,5 +27,5 @@ pca_seurat <- function(input){
    
    image4 <- Seurat::DimPlot(image4, group.by = "RNA_snn_res.0.3", label = TRUE)
    
-   ggplot2::ggsave(image4, file = "dimplot.png")
+   ggplot2::ggsave(image4, file = "dimplot.png", width = 12, height = 10)
 }
